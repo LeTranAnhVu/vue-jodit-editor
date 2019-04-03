8c450d1ba6339431f69ab043f9939bda25639d60
Lê Trần Anh Vũ <letrananhvu.developer@gmail.com>
2019-04-03 13:39:24 +0700

    done for common jodit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="vue-logo" alt="Vue logo" src="./assets/logo.png">
+    <section class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Edit</h3>
+          <textarea id="editor" name="editor" @change="getText"></textarea>
+          <button class="mr-auto btn btn-outline-dark mt-2" @click="getText">Render</button>
+        </div>
+        <div class="col-md-6">
+          <h3>Result</h3>
+          <article id="result"></article>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Jodit from 'jodit/build/jodit.min.js'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {},
+    data () {
+      return {
+        text: '',
+        o : {}
+      }
+    },
+    mounted () {
+      this.o = new Jodit('#editor', {
+        'uploader': {
+          'insertImageAsBase64URI': true,
+          'toolbarButtonSize': 'large',
+          'direction': 'ltr'
+        },
+      })
+
+    },
+    methods: {
+      getText () {
+        const texto = this.o.getEditorValue();
+        let resHTML = this.$el.querySelector('#result');
+        resHTML.innerHTML =texto ;
+      },
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  #editor {
+    width: 70%;
+  }
+  #result {
+    border: 1px #ddd solid ;
+    min-height: 300px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
+import '../node_modules/jquery/dist/jquery.min';
+import '../node_modules/popper.js/dist/popper.min';
+import  '../node_modules/bootstrap/dist/js/bootstrap.min';
+import '../node_modules/jodit/build/jodit.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
